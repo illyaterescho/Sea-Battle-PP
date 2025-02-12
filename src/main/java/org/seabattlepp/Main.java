258fe8d9b95709f3383f15caebcad073e052b8f9
Illa Tereshko <illyaterescho@knu.ua>
2025-02-12 15:58:41 +0200

    Add Files

--- src/main/java/org/seabattlepp/Main.java
+++
@@ -1,8 +0,0 @@
-package org.seabattlepp;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Sea Battle project is ready for development!");
-
-    }
-}
